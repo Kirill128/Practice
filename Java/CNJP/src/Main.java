b78bc3c36b7af3ef8ac63c08afd5f5eb09b8fe16
Kiriill <liw.liw510@gmail.com>
2020-09-24 19:28:33 +0300

    Prelast commit cisco lab

--- Java/CNJP/src/Main.java
+++ Java/CNJP/src/Main.java
@@ -8,7 +8,7 @@ public class Main
         System.out.println("Input num of users:");
         int UserQuantity=in.nextInt();
         System.out.println("Input speed:");
-        String Speed=in.nextInt();
+        int Speed=in.nextInt();
         System.out.println("Input moneu:");
         int Moneu=in.nextInt();
         LinkedList<String[]> database=getDatabase("/home/kirill/Practice/Java/CNJP/Database.txt");
@@ -21,36 +21,65 @@ public class Main
             System.out.println("Database error!");
         }
     }
-    public static void getFromDatabase(LinkedList<String[]> list,int quantity,String speed,int moneu)
+    public static void getFromDatabase(LinkedList<String[]> list,int quantity,int speed,int moneu)
     {
+
         int a=list.size();
         int b=list.getLast().length;
         ListIterator<String[]> iter=list.listIterator();
-        String [][] database= new String [a][b];
+        String [] nameBase= new String [a];
+        int [][] dataBase=new int[a][b-1];
         for(int i=0;iter.hasNext();i++)
         {
             String [] str=iter.next();
-            for(int j=0;j<b;j++)
+            nameBase[i]=str[0];
+            for(int j=0;j<b-1;j++)
             {
-                database[i][j]=str[j];
+                dataBase[i][j]=Iteger.valueOf(str[j+1]);
             }
         }
+        int routerCost=0,switchCost=0,cableCost=0,freePortsThisRout=0,quanSwitch=0,resCost=0;
         int firstSwitch=8;
         int firstCable=17;
-        for(int rout=0,m=0,q=0;rout<8;i++)
+
+        for(int rout=0;rout<8;rout++)
         {   
-            m=database[i][5];
-            if(database[i][4]==speed )
-                for(int switch=firstSwitch;switch<17;i++)
-                {
-                    while()
-                    {    
-                        for(int cable=firsCable;cable<19;cable++ )
+
+            if(dataBase[rout][3]>=speed && dataBase[rout][4]<moneu)
+            {
+                routerCost=dataBase[rout][4];
+                resCost=routerCost;
+                freePortsThisRout=dataBase[rout][1]-1;
+                for(int swich=firstSwitch,freePortsThisSwich;swich<17;i++)
+                {            
+                    if(dataBase[swich][3]>=speed )
+                    {
+                        freePortsThisSwich=dataBase[swich][1]-2;
+                        quanSwitch=(quantity-freePortsThisRout)/freePortsThisSwich;
+                        if((quantity-freePortsThisRout)%freePortsThisSwich!=0)
+                            quanSwitch++;
+
+                        switchCost=(quanSwitch*dataBase[swich][4]);
+                        resCost+=switchCost;
+
+                        if(resCost<moneu)
                         {
-                            
+                            for(int cable=firsCable;cable<19;cable++ )
+                            {
+                                cableCost=dataBase[cable][4]*(quanSwitch+1);
+                                resCost+=cableCost;
+                                if(dataBase[cable][3]>=speed && resCost<=moneu )
+                                {
+
+                                }
+                            }
                         }
-                    }
+
+                    }    
                 }
+
+            }
+            
         }
     }
     public static  LinkedList<String[]> getDatabase(String path)
